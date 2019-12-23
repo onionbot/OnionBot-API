05cd2261e096c9c2ca81fa68a5324dcb1bfa032a
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2019-12-23 22:51:44 +0000

    Rebuilt Adafruit thermal imaging as series of OOP functions

--- thermal_image.py
+++ thermal_image.py
@@ -1,7 +1,3 @@
-"""This example is for Raspberry Pi (Linux) only!
-   It will not work on microcontrollers running CircuitPython!"""
-
-
 import os
 import math
 import time
@@ -15,22 +11,10 @@ import adafruit_mlx90640
 
 INTERPOLATE = 10
 
-# MUST et I2C freq to 1MHz in /boot/config.txt
-i2c = busio.I2C(board.SCL, board.SDA)
-
-#low range of the sensor (this will be black on the screen)
-MINTEMP = 20. #-40
-#high range of the sensor (this will be white on the screen)
-MAXTEMP = 50. #300
-
+MINTEMP = 20. #-40 #low range of the sensor (this will be black on the screen)
+MAXTEMP = 50. #300 #high range of the sensor (this will be white on the screen)
 SCALE = 25
 
-# set up display
-os.environ['SDL_FBDEV'] = "/dev/fb0"
-os.environ['SDL_VIDEODRIVER'] = "fbcon"
-pygame.init()
-screen = pygame.display.set_mode((24*SCALE,32*SCALE))
-print(pygame.display.Info())
 
 #the list of colors we can choose from
 heatmap = (
@@ -48,28 +32,13 @@ COLORDEPTH = 1000
 
 colormap = [0] * COLORDEPTH
 
-#some utility functions
-def constrain(val, min_val, max_val):
-    return min(max_val, max(min_val, val))
 
-def map_value(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-def gaussian(x, a, b, c, d=0):
-    return a * math.exp(-(x - b)**2 / (2 * c**2)) + d
-
-def gradient(x, width, cmap, spread=1):
-    width = float(width)
-    r = sum([gaussian(x, p[1][0], p[0] * width, width/(spread*len(cmap))) for p in cmap])
-    g = sum([gaussian(x, p[1][1], p[0] * width, width/(spread*len(cmap))) for p in cmap])
-    b = sum([gaussian(x, p[1][2], p[0] * width, width/(spread*len(cmap))) for p in cmap])
-    r = int(constrain(r*255, 0, 255))
-    g = int(constrain(g*255, 0, 255))
-    b = int(constrain(b*255, 0, 255))
-    return r, g, b
-
-for i in range(COLORDEPTH):
-    colormap[i] = gradient(i, COLORDEPTH, heatmap)
+# set up display
+os.environ['SDL_FBDEV'] = "/dev/fb0"
+os.environ['SDL_VIDEODRIVER'] = "fbcon"
+pygame.init()
+screen = pygame.display.set_mode((24*SCALE,32*SCALE))
+print(pygame.display.Info())
 
 pygame.mouse.set_visible(False)
 screen.fill((255, 0, 0))
@@ -79,28 +48,99 @@ pygame.display.update()
 sensorout = pygame.Surface((32, 24))
 
 
-#initialize the sensor
-mlx = adafruit_mlx90640.MLX90640(i2c)
-print("MLX addr detected on I2C, Serial #", [hex(i) for i in mlx.serial_number])
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_32_HZ
-print(mlx.refresh_rate)
-print("Refresh rate: ", pow(2, (mlx.refresh_rate-1)), "Hz")
+class THERMAL_CAMERA(object):
+    """Save image to file"""
 
-frame = [0] * 768
-while True:
-    try: 
+
+    def __init__(self, i2c=None):
+
+        self.latest_frame = None
+
+        if i2c is None:
+            # MUST et I2C freq to 1MHz in /boot/config.txt
+            i2c = busio.I2C(board.SCL, board.SDA)
+        
+        #initialize the sensor
+        mlx = adafruit_mlx90640.MLX90640(i2c)
+        print("MLX addr detected on I2C, Serial #", [hex(i) for i in mlx.serial_number])
+        mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_32_HZ
+        print(mlx.refresh_rate)
+        print("Refresh rate: ", pow(2, (mlx.refresh_rate-1)), "Hz")
+
+        self.mlx = mlx
+
+
+    def _constrain(self, val, min_val, max_val):
+        return min(max_val, max(min_val, val))
+
+
+    def _map_value(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+    def _gaussian(self, x, a, b, c, d=0):
+        return a * math.exp(-(x - b)**2 / (2 * c**2)) + d
+
+
+    def _gradient(self, x, width, cmap, spread=1):
+        width = float(width)
+        r = sum([self._gaussian(x, p[1][0], p[0] * width, width/(spread*len(cmap))) for p in cmap])
+        g = sum([self._gaussian(x, p[1][1], p[0] * width, width/(spread*len(cmap))) for p in cmap])
+        b = sum([self._gaussian(x, p[1][2], p[0] * width, width/(spread*len(cmap))) for p in cmap])
+        r = int(self._constrain(r*255, 0, 255))
+        g = int(self._constrain(g*255, 0, 255))
+        b = int(self._constrain(b*255, 0, 255))
+        return r, g, b
+
+
+    def _visualise(self, img):
+        
+        img_surface = pygame.image.fromstring(img.tobytes(), img.size, img.mode)
+        pygame.transform.scale(img_surface.convert(), screen.get_size(), screen)
+        pygame.display.update()
+        print("Completed 2 frames in %0.2f s (%d FPS)" %
+              (time.monotonic()-stamp, 1.0 / (time.monotonic()-stamp)))
+
+
+    def capture_frame(self):
+
+        frame = [0] * 768
         stamp = time.monotonic()
         try:
-            mlx.getFrame(frame)
+            self.mlx.getFrame(frame)
         except ValueError:
+            print("ValueError: Retrying...")
             continue        # these happen, no biggie - retry
 
         print("Read 2 frames in %0.2f s" % (time.monotonic()-stamp))
 
+        self.latest_frame = frame
+        self.latest_stamp = stamp
+
+
+    def get_latest_temperature(self):
+        
+        frame = self.latest_frame 
+
+        if frame != None:
+            h = 12
+            w = 16        
+            t = self.frame[h*32 + w]
+            return t 
+
+
+    def save_latest_jpg(self, path, visualise=False):
+
+        frame = self.latest_frame 
+        file_name = self.latest_stamp    
+        
+        for i in range(COLORDEPTH):
+            colormap[i] = self._gradient(i, COLORDEPTH, heatmap)
+
         pixels = [0] * 768
         for i, pixel in enumerate(frame):
-            coloridx = map_value(pixel, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1)
-            coloridx = int(constrain(coloridx, 0, COLORDEPTH-1))
+            coloridx = self._map_value(pixel, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1)
+            coloridx = int(self._constrain(coloridx, 0, COLORDEPTH-1))
             pixels[i] = colormap[coloridx]
 
         for h in range(24):
@@ -108,19 +148,41 @@ while True:
                 pixel = pixels[h*32 + w]
                 sensorout.set_at((w, h), pixel)
 
-        #pixelrgb = [colors[constrain(int(pixel), 0, COLORDEPTH-1)] for pixel in pixels]
         img = Image.new('RGB', (32, 24))
         img.putdata(pixels)
         img = img.resize((32*INTERPOLATE, 24*INTERPOLATE), Image.BICUBIC)
         img = img.transpose(method=Image.ROTATE_90)
         img = img.transpose(method=Image.FLIP_LEFT_RIGHT)
-        img_surface = pygame.image.fromstring(img.tobytes(), img.size, img.mode)
-        pygame.transform.scale(img_surface.convert(), screen.get_size(), screen)
-        pygame.display.update()
-        print("Completed 2 frames in %0.2f s (%d FPS)" %
-              (time.monotonic()-stamp, 1.0 / (time.monotonic()-stamp)))
+
+        file_path = path+"/"+file_name+".jpg"
+        img.save(file_path)
+
+
+        if visualise==True:
+            self._visualise(img)
+
+
+
+    def save_latest_csv(self, path):
+
+        frame = self.latest_frame 
+        file_name = self.latest_stamp
+
+        data_string = []
+
+        if frame != None:        
+            for h in range(24):
+                for w in range(32):
+                    t = self.frame[h*32 + w]
+                    data_string.append("%0.1f, " % t, end="")
+            file_path = path+"/"+file_name+".csv"
+            file = open(file_path)
+            file.write(data_string)
+            file.close()
+
+
+
+
+
+
     
-    except KeyboardInterrupt:
-        print ("Exited cleanly")
-        sys.exit()
-        
