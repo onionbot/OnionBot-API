9ee5dbf64b2bd4403b08384711259b81e7ac3658
tompb <tom@personalboardroom.com>
2019-12-29 01:02:33 +0000

    Update app.py

--- onionserver/app.py
+++ onionserver/app.py
@@ -7,13 +7,13 @@ ctrl = control.CONTROL()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  # response.headers.add('Access-Control-Allow-Methods', 'GET,POST)
   return response
 
 def index():
