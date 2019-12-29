714bdd2ea9310ab7f4dea9a775072dff2aab6da9
bencobley <bc1916@ic.ac.uk>
2019-12-29 00:20:17 +0000

    initial work on onionserver

---
+++ onionserver/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from .. import control
+
+ctrl = control.CONTROL()
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if request.args.get('action') == 'set_hob':
+        return ctrl.set_hob(request.args.get('temp')) 
+    # return request.args.get('action')
+
+if __name__ == '__main__':
+    # TODO: add authentication 
+    app.run(debug=True, host='0.0.0.0')
+
