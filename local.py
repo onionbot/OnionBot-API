d0eaa47ce4a461d456cfe8decc0348a97d21c764
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2019-12-30 14:14:10 +0000

    Revert "pass through function success messages"
    This reverts commit 2a72d460dc787b2e7a1dde35d8c12b8854735835.

--- local.py
+++ local.py
@@ -2,56 +2,25 @@
 
 
 
-import logging
-import threading
-import time
+import cloud etc
 
 
 class LOCAL(object):
 
     def __init__(self):
-        
-        self.latest_meta = None
-        self.stop_flag = False
+        pass
 
 
-    def start(self, session_name):
-        """Start logging"""
-
-        self.session_name = session_name
-
-        def thread_function(name):
-
-            logging.info("Thread %s: starting", name)
-            
-            while self.stop_flag is False:
-
-                print ("Onionlog")
-                time.sleep(2)
-            
-            logging.info("Thread %s: finishing", name)
-        
-
-        format = "%(asctime)s: %(message)s"
-        logging.basicConfig(format=format, level=logging.INFO,
-                            datefmt="%H:%M:%S")       
-        
-        logging.info("Main    : before creating thread")
-        my_thread = threading.Thread(target=thread_function, args=(1,))
-        logging.info("Main    : before running thread")
-        my_thread.start()
-        logging.info("Main    : wait for the thread to finish")
-        my_thread.join()
-        logging.info("Main    : all done")
+    def start(self):
+        """Sends start command to Pi"""
 
 
         return 1
 
 
     def stop(self):
-        """Stop logging"""
+        """Sends stop command to Pi"""
 
-        self.stop_flag = True
 
         return 1 
 
@@ -59,7 +28,7 @@ class LOCAL(object):
     def get_latest_meta(self):
         """Returns cloud filepath of latest meta.json - includes path location of images"""
 
-        return self.latest_meta
+
 
     def get_labels(self):
         """Returns cloud filepath of labels.json - includes available image labels"""
@@ -95,16 +64,3 @@ class LOCAL(object):
         local.set_temperature_setpoint(value)
 
         return 1
-
-
-    def set_camera_frame_rate(self, value):
-        """Command to change camera targe refresh rate"""
-
-        return local.set_camera_frame_rate(value)
-
-
-local = LOCAL()
-
-local.start("test")
-
-
