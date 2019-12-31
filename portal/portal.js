4f6ecf6e4ded66b67300371e5ed8e4f84807d1e1
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2019-12-31 10:57:49 +0000

    Initial implementation of functions

--- portal/portal.js
+++ portal/portal.js
@@ -1,33 +1,106 @@
-"use strict";
+'use strict';
 
-$('#btn-set-temp').on('click', function() {
-  $.ajax({
-    type: "POST",
-    url: "http://192.168.0.73:5000/",
-    data: {
-      action: "set_hob",
-      temp: $("#temp").val()
-    },
-    success: function(data) {
-      alert(data);
-    },
-  });
+// $('#btn-set-temp').on('click', function() {
+//   $.ajax({
+//     type: 'POST',
+//     url: 'http://192.168.0.73:5000/',
+//     data: {
+//       action: 'set_hob',
+//       temp: $('#temp').val()
+//     },
+//     success: function(data) {
+//       alert(data);
+//     },
+//   });
+// });
+
+
+$(document).ready(function() {
+  
+
+  init() {
+    var dropdownOptions;
+    // call to python script, save repsonse to dropdownOptions 
+      // in callback function (once response has been obtained):
+      // populate dropdown with jQuery
+
+  }
+
+
 });
 
 
-function myFn(arg1, arg2) {
-  alert(arg1);
-  $("#btn-set-temp").html("button");
-  $(".btn").html("button");
+$('#start').on('click', function() {
+  
+  set('set_chosen_labels', $('#labels').val());
+  // Show hidden boxes
+  execute('start');
+  // Refresh stuff
+  setInterval(refresh, 500);
+
 }
 
-
 function refresh() {
-  $.getJSON("url goes here ?key=skdhflskfhldf", function(data) {
+  
+  var meta_url; 
+  meta_url = get('get_latest_meta');
+  $.getJSON(meta_url, function(data) {
     // data is a js object 
-    console.log(data.hobtemp);
+    
+    $('#session_name').html(data.session_name);
+    $('#label').html(data.label);
+    $('#camera_prediction').html(data.camera_prediction);
+    $('#thermal_prediction').html(data.thermal_prediction);
+    $('#measurement_id').html(data.measurement_id);
+    $('#time_stamp').html(data.time_stamp);
+    $('#temperature').html(data.temperature);
+    $('#camera_image').src = data.camera_filepath;
+    $('#thermal_image').src = data.thermal_filepath;
+
   });
 }
 
 
-setInterval(refresh, 500);
+
+function set(function_name, argument) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://192.168.0.73:5000/',
+    data: {
+      func: function_name,
+      arg: argument
+    },
+    success: function(confirmation) {
+      return confirmation;
+    },
+  });
+}
+
+function get(function_name) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://192.168.0.73:5000/',
+    data: {
+      func: function_name,
+    },
+    success: function(data) {
+      return data;
+    },
+  });
+}
+
+
+function execute(function_name) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://192.168.0.73:5000/',
+    data: {
+      func: function_name,
+    },
+    success: function(confirmation) {
+      return confirmation;
+    },
+  });
+}
+
+
