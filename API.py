a07d4ca9730bc564a2a4b4324ed9d393dead870f
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-01-11 12:50:31 +0000

    renamed app to API

--- API.py
+++ API.py
@@ -1,6 +1,6 @@
 
-from API import ONIONAPI
-api = ONIONAPI()
+from main import ONIONBOT
+bot = ONIONBOT()
 
 from flask import Flask
 from flask import request
@@ -16,59 +16,59 @@ def index():
     """Index is run automatically on init by flask"""
     
     # if request.form['action'] == 'set_hob':
-    #     return api.set_hob(request.form['temp'])
+    #     return bot.set_hob(request.form['temp'])
     # # return request.args.get('action')
 
     if request.form['action'] == 'start':
-        return api.start(request.form['value'])
+        return bot.start(request.form['value'])
 
 
     if request.form['action'] == 'stop':
-        return api.stop()
+        return bot.stop()
 
 
     if request.form['action'] == 'get_latest_meta':
-        return api.get_latest_meta()
+        return bot.get_latest_meta()
 
 
     if request.form['action'] == 'get_chosen_labels':
-        return api.get_chosen_labels()
+        return bot.get_chosen_labels()
 
 
     if request.form['action'] == 'set_chosen_labels':
-        return api.set_chosen_labels(request.form['value'])
+        return bot.set_chosen_labels(request.form['value'])
 
 
     if request.form['action'] == 'set_active_label':
-        return api.set_active_label(request.form['value'])
+        return bot.set_active_label(request.form['value'])
 
 
     if request.form['action'] == 'set_active_model':
-        return api.set_active_model(request.form['value'])
+        return bot.set_active_model(request.form['value'])
 
 
     if request.form['action'] == 'get_temperature_setpoint':
-        return api.get_temperature_setpoint()
+        return bot.get_temperature_setpoint()
 
 
     if request.form['action'] == 'get_camera_frame_rate':
-        return api.get_camera_frame_rate()
+        return bot.get_camera_frame_rate()
 
 
     if request.form['action'] == 'set_hob_setpoint':
-        return api.set_hob_setpoint(request.form['value'])
+        return bot.set_hob_setpoint(request.form['value'])
 
 
     if request.form['action'] == 'set_camera_sleep':
-        return api.set_camera_sleep(request.form['value'])
+        return bot.set_camera_sleep(request.form['value'])
 
 
     if request.form['action'] == 'get_all_labels':
-        return api.get_all_labels()
+        return bot.get_all_labels()
 
 
     if request.form['action'] == 'get_all_models':
-        return api.get_all_models()
+        return bot.get_all_models()
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0')
