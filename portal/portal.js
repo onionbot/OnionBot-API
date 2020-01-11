50fde434d2c6fc97472ac19af5b7b0b5991b2150
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-01-11 19:39:34 +0000

    Reorganise

--- portal/portal.js
+++ portal/portal.js
@@ -9,164 +9,168 @@ var update_interval = 500;
 /*******************************************************/
 
 
+// INTERFACE WITH API
 
 function set(function_name, argument) {
-  $.ajax({
-    type: 'POST',
-    url: endpoint_url,
-    data: {
-      action: function_name,
-      value: argument
-    },
-    success: function(confirmation) {
-    },
-  });
+    $.ajax({
+        type: 'POST',
+        url: endpoint_url,
+        data: {
+            action: function_name,
+            value: argument
+        },
+        success: function(confirmation) {},
+    });
 }
 
 
 function get(function_name, callback) {
-  $.ajax({
-    type: 'POST',
-    url: endpoint_url,
-    data: {
-      action: function_name,
-    },
-    success: callback 
-  });
+    $.ajax({
+        type: 'POST',
+        url: endpoint_url,
+        data: {
+            action: function_name,
+        },
+        success: callback
+    });
 }
+
+
+// ASYNC UPDATE PAGE
 
 function update() {
 
+    get("get_latest_meta", function(data) {
+        // data is a js object 
 
-  get("get_latest_meta", function( data ) {
-    // data is a js object 
+        data = JSON.parse(data);
 
-    data = JSON.parse(data);
-    
-    $('#session-name').html(data.attributes.session_name);
-    $('#active-label').html(data.attributes.active_label);
-    $('#active-model').html(data.attributes.active_model);
-    $('#camera-prediction').html(data.attributes.camera_prediction);
-    $('#thermal-prediction').html(data.attributes.thermal_prediction);
-    $('#measurement-id').html(data.attributes.measurement_id);
-    $('#time-stamp').html(data.attributes.time_stamp);
-    $('#temperature').html(data.attributes.temperature);
-    $('#camera-filepath').html(data.attributes.camera_filepath);
-    $('#thermal-filepath').html(data.attributes.thermal_filepath);
-    $('#camera-filepath').attr("href", data.attributes.camera_filepath);
-    $('#thermal-filepath').attr("href", data.attributes.thermal_filepath);
-    $('#camera-image').attr("src", data.attributes.camera_filepath);
-    $('#thermal-image').attr("src", data.attributes.thermal_filepath);
-    $('#hob-setpoint').attr("placeholder", data.attributes.hob_setpoint);
-    $('#camera-sleep').attr("placeholder", data.attributes.camera_sleep);
-  });
-  
+        $('#session-name').html(data.attributes.session_name);
+        $('#active-label').html(data.attributes.active_label);
+        $('#active-model').html(data.attributes.active_model);
+        $('#camera-prediction').html(data.attributes.camera_prediction);
+        $('#thermal-prediction').html(data.attributes.thermal_prediction);
+        $('#measurement-id').html(data.attributes.measurement_id);
+        $('#time-stamp').html(data.attributes.time_stamp);
+        $('#temperature').html(data.attributes.temperature);
+        $('#camera-filepath').html(data.attributes.camera_filepath);
+        $('#thermal-filepath').html(data.attributes.thermal_filepath);
+        $('#camera-filepath').attr("href", data.attributes.camera_filepath);
+        $('#thermal-filepath').attr("href", data.attributes.thermal_filepath);
+        $('#camera-image').attr("src", data.attributes.camera_filepath);
+        $('#thermal-image').attr("src", data.attributes.thermal_filepath);
+        $('#hob-setpoint').attr("placeholder", data.attributes.hob_setpoint);
+        $('#camera-sleep').attr("placeholder", data.attributes.camera_sleep);
+    });
+
 }
 
 
+// EVENT LISTENERS FOR PAGE INTERACTION
+
 $(document).ready(function() {
-  
-  $("#stop").hide();
 
-  // Event listeners for page interactions 
+    $("#stop").hide();
 
-  get("get_all_labels", function( data ) {
-    // data is a js object 
+    get("get_all_labels", function(data) {
+        // data is a js object 
 
-    var label_json
-    label_json = data //JSON.parse(data);
+        var label_json
+        label_json = data //JSON.parse(data);
 
-    let dropdown = $('#select-labels');
+        let dropdown = $('#select-labels');
 
-    dropdown.empty();
+        dropdown.empty();
+        dropdown.append('<option selected="true" disabled>Select labels</option>');
+        dropdown.prop('selectedIndex', 0);
 
-    dropdown.append('<option selected="true" disabled>Select labels</option>');
-    dropdown.prop('selectedIndex', 0);
+        // Populate dropdown with list of provinces
+        var dataJSON = JSON.parse(data);
 
-    // Populate dropdown with list of provinces
-    var dataJSON = JSON.parse(data);
+        $.each(dataJSON, function(key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.label).text(entry.label));
+        });
 
-    $.each(dataJSON, function (key, entry) {
-      dropdown.append($('<option></option>').attr('value', entry.label).text(entry.label));
-    });
-
-    
-  });
-
-
-  get("get_all_models", function( data ) {
-    // data is a js object 
-
-    var label_json
-    label_json = data //JSON.parse(data);
-
-    let dropdown = $('#select-model');
-
-    dropdown.empty();
-
-    dropdown.append('<option selected="true" disabled>Select model</option>');
-    dropdown.prop('selectedIndex', 0);
-
-    // Populate dropdown with list of provinces
-    var dataJSON = JSON.parse(data);
-
-    $.each(dataJSON, function (key, entry) {
-      dropdown.append($('<option></option>').attr('value', entry.label).text(entry.label));
-    });
-
-    
-  });
-
-
-  $('#label-button1').on('click', function() {
-    set('set_active_label', $('#label-button1').text());
-  });
-  $('#label-button2').on('click', function() {
-    set('set_active_label', $('#label-button2').text());
-  });
-  $('#label-button3').on('click', function() {
-    set('set_active_label', $('#label-button3').text());
-  });
-
-
-  $('#start').on('click', function() {
-    
-    set('start', $('#session-id').val());
-
-    set('set_chosen_labels', $('#select-labels').val());
-
-
-    $("#start").hide();
-    $("#stop").show();
-
-    // Refresh page
-    setInterval(update, update_interval);
-  });
-
-  $('#stop').on('click', function() {
-    
-    get("stop", function( foo ) {
-      console.log("Stopping")
-
-      $("#stop").hide();
-      $("#start").show();
-      clearInterval(update)
 
     });
 
-   });
+    $('#start').on('click', function() {
 
-  $('#hob-setpoint-button').on('click', function() {
-    console.log("test")
-    set('set_hob_setpoint', $('#hob-setpoint').val());
-  });
-  
+        set('start', $('#session-id').val());
 
-  $('#camera-sleep-button').on('click', function() {
-    console.log("test")
-    set('set_camera_sleep', $('#camera-sleep').val());
-  });
+        set('set_chosen_labels', $('#select-labels').val());
+
+        $("#start").hide();
+        $("#stop").show();
+
+        // Refresh page
+        setInterval(update, update_interval);
+    });
+
+
+    get("get_all_models", function(data) {
+        // data is a js object 
+
+        var label_json
+        label_json = data //JSON.parse(data);
+
+        let dropdown = $('#select-model');
+
+        dropdown.empty();
+
+        dropdown.append('<option selected="true" disabled>Select model</option>');
+        dropdown.prop('selectedIndex', 0);
+
+        // Populate dropdown with list of provinces
+        var dataJSON = JSON.parse(data);
+
+        $.each(dataJSON, function(key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.label).text(entry.label));
+        });
+
+
+    });
+
+
+    $('#label-button1').on('click', function() {
+        set('set_active_label', $('#label-button1').text());
+    });
+    $('#label-button2').on('click', function() {
+        set('set_active_label', $('#label-button2').text());
+    });
+    $('#label-button3').on('click', function() {
+        set('set_active_label', $('#label-button3').text());
+    });
+
+    $('#select-model-button').on('click', function() {
+
+        set('set_chosen_model', $('#select-model').val());
+
+    });
+
+    $('#stop').on('click', function() {
+
+        get("stop", function(foo) {
+            console.log("Stopping")
+
+            $("#stop").hide();
+            $("#start").show();
+            clearInterval(update)
+
+        });
+
+    });
+
+    $('#hob-setpoint-button').on('click', function() {
+        console.log("test")
+        set('set_hob_setpoint', $('#hob-setpoint').val());
+    });
+
+
+    $('#camera-sleep-button').on('click', function() {
+        console.log("test")
+        set('set_camera_sleep', $('#camera-sleep').val());
+    });
 
 
 });
-
