daa0644ed82360bddafed1d6951947107548dcf0
bencobley <bc1916@ic.ac.uk>
2020-04-11 11:51:18 +0100

    Improved instuctions

--- utils/servo_install.py
+++ utils/servo_install.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
@@ -6,8 +8,9 @@ import time
 
 servo = Servo()
 
-_ = input("Remove servo assembly from hob body. Press enter to continue ")
+_ = input("Remove servo assembly from hob body. Press enter to continue _")
 print("Aligning servo...")
 servo.rotate(350)
-_ = input("Rotate hob knob anticlockwise to extreme point. Press enter to continue ")
-_ = input("Reattatch servo assembly to hob. Press enter to finish ")
+print("Rotate hob knob anticlockwise to 0, then rotate forward aprrox 10 degrees")
+_ = input(" Press enter to continue _")
+_ = input("Reattatch servo assembly to hob. Press enter to finish _")
