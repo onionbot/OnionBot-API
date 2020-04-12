a940e5d39c87d1dfb2a32796927468ff0921201a
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-04-12 21:13:57 +0100

    Yay! Added file names

--- camera.py
+++ camera.py
@@ -10,49 +10,37 @@ from picamera import PiCamera
 
 
 class Camera(object):
-    def __init__(self):  # , *args, **kwargs
-
-        logging.info("Initialising camera...")
-
-        sleep(1)
+    def __init__(self): 
 
         self.file_queue = JoinableQueue(1)
         self._launch()
 
     def _worker(self):
 
-        logging.debug("Initialising worker...")
+        logging.info("Initialising camera")
 
         camera = PiCamera()
         camera.rotation = 180
         camera.zoom = (0.05, 0.0, 0.75, 0.95)
         camera.resolution = (1024, 768)
 
-        
-
-        logging.debug("Waiting for start command...")
-
-
         while True:
-            self.file_queue.get(block=True)
+            file_name = self.file_queue.get(block=True)
 
-            logging.debug("Capture process started")
-            self.camera.capture("test.jpg")  # , resize=(240, 240)
-            logging.debug("Capture process ended")
+            logging.debug("Capturing image")
+            camera.capture(file_name, resize=(240, 240))
 
             self.file_queue.task_done()
 
     def start(self, file_name):
-        logging.debug("Calling start...")
+        logging.debug("Calling start")
         self.file_queue.put(file_name, block=True)
 
     def join(self):
-        logging.debug("Calling join...")
-
+        logging.debug("Calling join")
         self.file_queue.join()
-        return "test.jpg"
 
     def _launch(self):
-        logging.debug("Launch called")
+        logging.debug("Initialising worker")
         p = mp.Process(target=self._worker)
         p.start()
