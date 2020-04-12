d8c00dec0742b6c9152f3d28b5efdbc36bc372bb
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-04-12 16:56:59 +0100

    Multiprocessing camera initial commit

--- camera.py
+++ camera.py
@@ -1,24 +1,40 @@
-import time
+import multiprocessing as mp
+from multiprocessing import JoinableQueue
+
 from time import sleep
 from picamera import PiCamera
 
 
 class Camera(object):
-    def __init__(self):
+    def __init__(self):  # , *args, **kwargs
 
         print("Initialising camera...")
 
         camera = PiCamera()
         camera.rotation = 180
         camera.zoom = (0.05, 0.0, 0.75, 0.95)
-
         camera.resolution = (1024, 768)
 
         # camera.start_preview()
         self.camera = camera
 
     def capture(self, file_path):
+        self.intray.put(file_path)
+        
 
-        self.camera.capture(file_path, resize=(240, 240))
+    def thread(self):
+        while True:
+            file_path = self.intray.get(block=True)
+            self.camera.capture(file_path, resize=(240, 240))
 
-        return file_path
+    def run(self):
+
+        self.intray = JoinableQueue(2)
+
+        p = mp.Process(target=self.thread)
+        p.start()
+
+
+if __name__ == "__main__":
+    a = A()
+    a.run()
