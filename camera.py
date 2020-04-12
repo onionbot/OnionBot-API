ba449b4386e603af7bd35d0ab901fc44770afe08
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-04-12 20:54:09 +0100

    MP refactor - will this work?

--- camera.py
+++ camera.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 import multiprocessing as mp
+from multiprocessing import JoinableQueue
 
 from time import sleep
 from picamera import PiCamera
@@ -13,27 +14,44 @@ class Camera(object):
 
         logging.info("Initialising camera...")
 
+        sleep(1)
+
+        self._launch()
+
+    def _worker(self):
+
+        logging.debug("Initialising worker...")
+
         camera = PiCamera()
         camera.rotation = 180
         camera.zoom = (0.05, 0.0, 0.75, 0.95)
         camera.resolution = (1024, 768)
 
-        # camera.start_preview()
-        self.camera = camera
+        self.file_queue = JoinableQueue(1)
 
-    def _worker(self, file_path):
-        logging.debug("Capture process started")
-        self.camera.capture("test.jpg") #, resize=(240, 240)
-        logging.debug("Capture process ended")
+        while True:
+            
+            logging.debug("Waiting for start command...")
+            self.file_queue.get(block=True)
 
+            logging.debug("Capture process started")
+            self.camera.capture("test.jpg")  # , resize=(240, 240)
+            logging.debug("Capture process ended")
 
-    def start(self, file_path):
-        logging.debug("Start called")
-        p = mp.Process(target=self._worker, args=(file_path, ))
-        p.start()
-        self.p = p
+            self.file_queue.task_done()
+
+    def start(self, file_name):
+        logging.debug("Calling start...")
+        self.file_queue.put(file_name, block=True)
 
     def join(self):
         logging.debug("Calling join...")
-        self.p.join()
+
+        self.file_queue.join()
         return "test.jpg"
+
+    def _launch(self):
+        logging.debug("Launch called")
+        p = mp.Process(target=self._worker)
+        p.start()
+        p.join()
