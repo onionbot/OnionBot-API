2f904d6b758db980c1dff02fe5fa269f78280dff
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-04-12 20:32:43 +0100

    Most basic camera mp setup !!!!

--- camera.py
+++ camera.py
@@ -3,7 +3,6 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 import multiprocessing as mp
-from multiprocessing import JoinableQueue
 
 from time import sleep
 from picamera import PiCamera
@@ -22,44 +21,19 @@ class Camera(object):
         # camera.start_preview()
         self.camera = camera
 
-    def capture ()
-        self.camera.capture("test.jpg") #, resize=(240, 240)
-
-
-
     def _worker(self, file_path):
         logging.debug("Capture process started")
-        logging.debug(file_path)
-        
-
-        logging.debug("Taking picture")
-
-        self.capture()
-
+        self.camera.capture("test.jpg") #, resize=(240, 240)
         logging.debug("Capture process ended")
 
-        # logging.debug("Captured, putting file path in queue")
-        # self.file_queue.put(file_path)
 
     def start(self, file_path):
         logging.debug("Start called")
-
-        # self.file_queue = JoinableQueue()
-
         p = mp.Process(target=self._worker, args=(file_path, ))
         p.start()
         self.p = p
-        # p.join()
 
     def join(self):
-        print(self.p.is_alive())
         logging.debug("Calling join...")
-
-        # logging.debug("Calling file join...")
-        # file_path = self.file_queue.join()
-        # logging.debug("File joined")
-        # file_queue.close()
-        # logging.debug("Queue closed")
         self.p.join()
-        print(self.p.is_alive())
         return "test.jpg"
