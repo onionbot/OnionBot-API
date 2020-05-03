9881f995ab5f9dbc36df053bd1d669e28890b436
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-05-03 10:04:42 +0100

    Created thread worker for servo

---
+++ control.py
@@ -0,0 +1,52 @@
+import logging
+from threading import Thread, Event
+from servo import Servo
+from time import sleep
+
+servo = Servo()
+
+
+class control(object):
+
+    def __init__(self):
+
+        self.quit_event = Event()
+
+        self.control_setpoint = 0
+
+    def _worker(self):
+
+        while True:
+
+            self.servo_setpoint = servo.get_setpoint()
+            self.servo_actual = servo.get_actual()
+
+            servo.update_setpoint(self.control_setpoint)
+
+            if self.quit_event.is_set():
+                logging.debug("Quitting control thread...")
+                break
+
+            sleep(0.1)
+
+    def start(self):
+        Thread(target=self._worker)
+
+    def quit(self):
+        self.quit_event.set()
+
+    def update_setpoint(self, target_setpoint):
+
+        self.control_setpoint = target_setpoint
+
+    def get_setpoint(self):
+
+        return self.servo_setpoint
+
+    def get_actual(self):
+
+        return self.servo_actual
+
+    def hob_off(self):
+
+        return servo.hob_off()
