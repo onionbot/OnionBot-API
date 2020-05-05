7caf5ce3e8faeafdcae3673a6d70f3d18798be8f
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-05-05 16:15:22 +0100

    Try new callback system

--- reset_button.py
+++ reset_button.py
@@ -3,20 +3,30 @@ from datetime import datetime
 
 pi = pigpio.pi()
 
-PIN = 23
+PIN = 21
 
 pi.set_mode(PIN, pigpio.INPUT)  # GPIO  4 as input
 pi.set_pull_up_down(PIN, pigpio.PUD_UP)
 pi.set_glitch_filter(PIN, 100)
 
 
-while True:
-    pi.wait_for_edge(user_gpio=PIN, edge=pigpio.FALLING_EDGE, wait_timeout=0)
-    print("Rising edge detected")
+timer = datetime.now()
+
+
+def pressed_callback(gpio, level, tick):
+    print("Falling edge detected")
+
+    global timer
     timer = datetime.now()
 
-    if pi.wait_for_edge(PIN, pigpio.RISING_EDGE, 5.0):
-        print("Falling edge detected")
-        print(datetime.now() - timer)
-    else:
-        print("wait for falling edge timed out")
+
+def unpressed_callback(gpio, level, tick):
+    print("Rising edge detected")
+
+    global timer
+    print(timer - datetime.now())
+
+
+falling = pi.callback(PIN, pigpio.FALLING_EDGE, pressed_callback)
+
+rising = pi.callback(PIN, pigpio.RISING_EDGE, unpressed_callback)
