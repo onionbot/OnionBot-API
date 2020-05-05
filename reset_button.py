9fdbda75a0f753f0f4df7b320076efc72a28f907
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-05-05 15:45:35 +0100

    Bug fixes

--- reset_button.py
+++ reset_button.py
@@ -3,17 +3,19 @@ from datetime import datetime
 
 pi = pigpio.pi()
 
-pi.set_mode(23, pigpio.INPUT)  # GPIO  4 as input
+PIN = 23
 
-pi.set_glitch_filter(23, 100)
+pi.set_mode(PIN, pigpio.INPUT)  # GPIO  4 as input
+pi.set_pull_up_down(PIN, pigpio.PUD_UP)
+pi.set_glitch_filter(PIN, 100)
 
 
 while True:
-    pi.wait_for_edge(user_gpio=23, edge=pigpio.FALLING_EDGE, timeout=0)
+    pi.wait_for_edge(user_gpio=PIN, edge=pigpio.FALLING_EDGE, timeout=0)
     print("Rising edge detected")
     timer = datetime.now()
 
-    if pi.wait_for_edge(23, pigpio.RISING_EDGE, 5.0):
+    if pi.wait_for_edge(PIN, pigpio.RISING_EDGE, 5.0):
         print("Falling edge detected")
         print(datetime.now() - timer)
     else:
