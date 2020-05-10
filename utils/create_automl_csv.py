18da2bc5752ed7ad2c2fb3ed04171da92add8dfc
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-05-10 19:13:13 +0100

    black formatting

--- utils/create_automl_csv.py
+++ utils/create_automl_csv.py
@@ -5,8 +5,8 @@ import csv
 session_ID = "monday1"
 sensor = "camera"
 label = "not_boiling"
-path = F"/Users/bencobley/Downloads/{session_ID}/{sensor}/{label}"
-csv_file = F"/Users/bencobley/Downloads/{session_ID}/{sensor}_{label}.csv"
+path = f"/Users/bencobley/Downloads/{session_ID}/{sensor}/{label}"
+csv_file = f"/Users/bencobley/Downloads/{session_ID}/{sensor}_{label}.csv"
 
 
 files = []
@@ -16,24 +16,30 @@ for (dirpath, dirnames, filenames) in walk(path):
     break
 
 
-
 total_count = 0
 skip_count = 0
-added_count = 0 
+added_count = 0
 
-with open(csv_file, mode='w') as label_file:
-    file_writer = csv.writer(label_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+with open(csv_file, mode="w") as label_file:
+    file_writer = csv.writer(
+        label_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+    )
 
     file_writer.writerow(["image_path[,label]"])
-    
+
     for filename in filenames:
         total_count += 1
         skip_count += 1
-        if skip_count == 4: 
-            file_writer.writerow([str(F"gs://onionbucket/logs/{session_ID}/{sensor}/{label}/{filename},{label}")])
+        if skip_count == 4:
+            file_writer.writerow(
+                [
+                    str(
+                        f"gs://onionbucket/logs/{session_ID}/{sensor}/{label}/{filename},{label}"
+                    )
+                ]
+            )
             skip_count = 0
             added_count += 1
 
 
-print (F"Added {added_count} files to csv")
-
+print(f"Added {added_count} files to csv")
