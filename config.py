db26404cc9c661b1e83c9cc2a93a3944bf356a77
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-06-04 10:57:04 +0100

    Troubleshooting empty config file

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ FILE = "/home/pi/onionbot/config.json"
 class Settings(object):
     def get_config(self, key):
 
-        with open(FILE, "r") as json_data_file:
+        with open(FILE) as json_data_file:
             config = json.load(json_data_file)
 
             settings = config["settings"]
@@ -18,7 +18,7 @@ class Settings(object):
 
     def set_config(self, key, value):
 
-        with open(FILE, "w") as json_data_file:
+        with open(FILE) as json_data_file:
             config = json.load(json_data_file)
 
             settings = config["settings"]
@@ -26,9 +26,11 @@ class Settings(object):
             if key in settings:
                 settings[key] = value
 
+                config["settings"] = settings
+
                 # Close file then dump new version of config
                 json_data_file.close()
-                with open(FILE, "w") as outfile:
+                with open(FILE) as outfile:
                     json.dump(settings, outfile)
             else:
                 raise KeyError("Settings key not found")
