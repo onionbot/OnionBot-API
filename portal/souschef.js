6818c0e11ae90c1484240a4cf5784899ec325690
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2020-06-05 18:33:02 +0100

    Upload sous API

---
+++ portal/souschef.js
@@ -0,0 +1,136 @@
+// -------------------------------------------------------
+// PORTAL.JS COPYRIGHT BEN COBLEY 2020 
+
+
+
+
+// -------------------------------------------------------
+// ---------------- APPLICATION VARIABLES ----------------
+
+
+var endpoint_url = 'http://192.168.0.1:5000/'
+if (!localStorage.ip_address) {
+    $('#IPmodal').modal('show');
+} else {
+    var endpoint_url = 'http://192.168.0.' + localStorage.ip_address + ':5000/';
+}
+
+var update_interval = 100;
+var connected = false;
+var previous_message = null;
+
+// ------------------ INTERFACE WITH API ----------------
+
+function set(function_name, argument) {
+    $.ajax({
+        type: 'POST',
+        url: endpoint_url,
+        data: {
+            action: function_name,
+            value: argument
+        },
+        success: function(confirmation) {},
+    });
+}
+
+
+function get(function_name, callback) {
+    $.ajax({
+        type: 'POST',
+        url: endpoint_url,
+        data: {
+            action: function_name,
+        },
+        success: callback,
+        error: connection_failed,
+    });
+
+}
+
+
+// ------------- UPDATE FREQUENTLY -------------
+
+function update() {
+
+    $('#ip-address-output').html(localStorage.ip_address);
+
+    get("get_screen_message", function(data) {
+        // data is a js object 
+        connection_success()
+
+        // Only refresh when new data
+        if (data == previous_message) {
+            // Update previous meta
+            previous_message = data
+        } else {
+            previous_message = data
+
+            $("#current-instruction").html(data)
+
+            
+            fitty.fitAll()
+
+
+        }
+
+    });
+}
+
+
+// ------------- UPDATE ON CONNECTION SUCCESS -------------
+
+
+function connection_success() {
+    $("#not-connected").hide();
+    $('#connected').show();
+}
+
+function connection_failed() {
+    $("#connected").hide();
+    $("#not-connected").show();
+}
+
+
+
+// ------------- UPDATE ON INITIAL PAGE LOAD -------------
+
+
+$(document).ready(function() {
+
+    $('#ip-address').attr("placeholder", localStorage.ip_address);
+
+    // Refresh page
+    setInterval(update, update_interval);
+
+});
+
+
+
+
+//---------- ASYNCHRONOUS EVENT PAGE LISTENERS  ----------
+
+$(document).ready(function() {
+
+    // API control
+
+    $('#ip-address-button').on('click', function() {
+        localStorage.ip_address = $('#ip-address-input').val();
+        $('#ip-address-output').html(localStorage.ip_address);
+        $('#ip-address-input').val('');
+        endpoint_url = 'http://192.168.0.' + localStorage.ip_address + ':5000/';
+    });
+
+    $('#next').on('click', function() {
+        set("next", function(foo) {});
+    });
+
+    $('#previous').on('click', function() {
+        set("previous", function(foo) {});
+    });
+
+    $('#stop').on('click', function() {
+        set("stop", function(foo) {});
+    });
+
+
+});
---
+++ souschef/sous_API.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+from souschef import SousChef
+
+import logging
+
+# Silence Flask werkzeug logger
+logger = logging.getLogger("werkzeug")
+logger.setLevel(logging.ERROR)  # Note! Set again below
+
+# Initialise SousChef
+sous = SousChef()
+sous.run()
+
+# Initialise flask server
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Index is run automatically on init by flask"""
+
+    if request.form["action"] == "get_message":
+        logger.debug("get_message called")
+        return sous.screen_message
+
+    if request.form["action"] == "next":
+        logger.debug("next called")
+        sous.next()
+        return "1"
+
+    if request.form["action"] == "previous":
+        logger.debug("previous called")
+        sous.previous()
+        return "1"
+
+    if request.form["action"] == "stop":
+        logger.debug("stop called")
+        sous.stop()
+        return "1"
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0, port=5001")
